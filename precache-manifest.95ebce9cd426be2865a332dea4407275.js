78ba01a359328163f3f416de1f3640e121ed84da
Sniadek <wojokarolg9@gmail.com>
2019-04-25 17:23:56 +0200

    add breakpoints in about section

--- precache-manifest.95ebce9cd426be2865a332dea4407275.js
+++ precache-manifest.95ebce9cd426be2865a332dea4407275.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "d82d2522f8a76e703ab1",
-    "url": "/static/js/main.d82d2522.chunk.js"
+    "revision": "8ca232c1b108798c85d5",
+    "url": "/static/js/main.8ca232c1.chunk.js"
   },
   {
     "revision": "b58e98cb4495572fafa3",
     "url": "/static/js/2.b58e98cb.chunk.js"
   },
   {
-    "revision": "d82d2522f8a76e703ab1",
+    "revision": "8ca232c1b108798c85d5",
     "url": "/static/css/main.b521d2fb.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.3aa5023c.chunk.css"
   },
   {
-    "revision": "d8fb28ae86a22488ce6f2a796ec67f97",
+    "revision": "601c873c65ed0d49801899da2552d60b",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1460ba289a8698cf60d5342efc69aec.js"
+  "/precache-manifest.95ebce9cd426be2865a332dea4407275.js"
 );
 
 workbox.clientsClaim();
